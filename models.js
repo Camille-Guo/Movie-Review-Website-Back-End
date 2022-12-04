f3032617cd57c29b5115a5d3198ca1d314c5ee43
Camille-Guo <zibin_guo@hotmail.com>
2022-12-04 15:47:22 -0500

    comment record schema

--- models.js
+++ models.js
@@ -1,3 +1,4 @@
+const { text } = require("body-parser");
 const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
   username: {
@@ -19,6 +20,47 @@ const UserSchema = new mongoose.Schema({
     required: false,
   },
 });
-//** */
+/* users */
 const User = mongoose.model("User", UserSchema);
 module.exports = User;
+
+const CommentRecordSchema = new mongoose.Schema({
+  recordId: {
+    type: String,
+    required: true,
+  },
+  movieId: {
+    type: String,
+    required: true,
+    unique: false,
+  },
+  userId: {
+    type: String,
+    required: true,
+    unique: false,
+  },
+  username: {
+    type: String,
+    required: true,
+  },
+  updateDate: {
+    type: Date,
+    default: Date.now,
+  },
+  rate: {
+    type: Number,
+    default: 0,
+    required: false,
+  },
+  content: {
+    contentId: {
+      type: String,
+    },
+    contentText: {
+      type: String,
+    },
+    require: false,
+  },
+});
+const CommentRecord = mongoose.model("CommentRecord", CommentRecordSchema);
+module.exports = CommentRecord;
