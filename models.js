f9320a686e192db5562d390bc0a0b19ea0b6e0ad
Camille-Guo <zibin_guo@hotmail.com>
2022-12-04 13:43:08 -0500

    test

--- models.js
+++ models.js
@@ -1,25 +1,24 @@
 const mongoose = require("mongoose");
 const UserSchema = new mongoose.Schema({
-    username: {
-        type: String,
-        required: true,
-    },
-    email:{
-        type: String,
-        required: true,
-        unique: [true,'Email Exist'],
-    },
-    password: {
-        type: String,
-        required: [true,'Please provide a password'],
-        unique: false,
-    },
-    avatar: {
-        type: Buffer,
-        required: false,
-    }
-
+  username: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: [true, "Email Exist"],
+  },
+  password: {
+    type: String,
+    required: [true, "Please provide a password"],
+    unique: false,
+  },
+  avatar: {
+    type: Buffer,
+    required: false,
+  },
 });
-
+//** */
 const User = mongoose.model("User", UserSchema);
 module.exports = User;
